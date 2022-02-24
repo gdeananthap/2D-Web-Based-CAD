547b2b3e270d04c5b15be0eab13b124a498d9345
gdeananthap <gdeanantha33@gmail.com>
2022-02-25 01:04:25 +0700

    initialize UI

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ window.onload = function init() {
     }
     
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
+    gl.clearColor( 1, 1, 1, 1 );
     gl.clear( gl.COLOR_BUFFER_BIT );
 
 
