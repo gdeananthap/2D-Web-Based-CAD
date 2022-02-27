4ba7f311c23857d1922e60cb0acb90c32a31c88b
nypnop <68521696+nypnop@users.noreply.github.com>
2022-02-27 15:47:41 +0700

    add drawing rect and square

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var canvas;
 var gl;
 
 var maxNumLines = 200;  
-var maxNumVertices  = 2 * maxNumLines;
+var maxNumVertices  = 3 * maxNumLines;
 var index_line = 0;
+var index_sq = 0;
 var index_polygon = 0;
 var first = true;
 
-var t, t1, t2;
+var t, t1, t2, t3, t4;
 var countPolygon = 0;
 var countIndices = [0];
 var start = [0];
@@ -72,6 +73,8 @@ window.onload = function init() {
                 gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer)
                 t1 = vec2(2*event.clientX/canvas.width-1, 
                     2*(canvas.height-event.clientY)/canvas.height-1);
+                console.log(t1[0]);
+
                 
             }
             else {
@@ -79,6 +82,7 @@ window.onload = function init() {
                 first = true;
                 t2 = vec2(2*event.clientX/canvas.width-1, 
                     2*(canvas.height-event.clientY)/canvas.height-1);
+                console.log(t2);
 
                 gl.bufferSubData(gl.ARRAY_BUFFER, 8*index_line, flatten(t1));
                 gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_line+1), flatten(t2));
@@ -93,10 +97,114 @@ window.onload = function init() {
                 gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_line-1), flatten(t));
                 render();
             }
-        // } else if (document.getElementById("square").checked) {
-            // function square
-        // } else if (document.getElementById("rectangle").checked) {
-            //function rectangle
+        } else if (document.getElementById("square").checked) {
+            gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer);
+            if(first) {
+                first = false;
+                gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer)
+                var x, y;
+                x = 2*event.clientX/canvas.width-1;
+                y = 2*(canvas.height-event.clientY)/canvas.height-1;
+                console.log(x,y);
+                t1 = vec2(x,y);
+                console.log(t1);
+                
+            }
+            else {
+                first = true;
+                var xf, xx, yy;
+                yy = 2*(canvas.height-event.clientY)/canvas.height-1;
+                xf = 2*event.clientX/canvas.width-1;
+                xx = x > xf ? t1[0] - Math.abs(t1[1]-yy) : t1[0] + Math.abs(t1[1]-yy);
+
+                t2 = vec2(xx,yy);
+                console.log(t2);
+                t3 = vec2(t1[0], t2[1]);
+                t4 = vec2(t2[0], t1[1]);
+                //console.log(t1,t2,t3,t4);
+
+                gl.bufferSubData(gl.ARRAY_BUFFER, 8*index_sq, flatten(t1));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_sq+1), flatten(t3));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_sq+2), flatten(t2));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_sq+3), flatten(t4));
+                gl.bindBuffer( gl.ARRAY_BUFFER, colorBuffer);
+                index_sq += 4;
+
+                colors = hexToVec4(document.getElementById("color").value);
+                
+                t = vec4(colors);
+
+                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-4), flatten(t));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-3), flatten(t));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-2), flatten(t));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-1), flatten(t));
+                render();
+            }
+        
+            // gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+            // if(first) {
+            //     first = false;
+            //     gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+                
+            //     t1 = vec2(2*event.clientX/canvas.width-1, 2*(canvas.height-event.clientY)/canvas.height-1);
+                
+            // } else {
+            //     first = true;
+            //     t2 = vec2(2*event.clientX/canvas.width-1, 2*(canvas.height-event.clientY)/canvas.height-1);
+            //     t3 = vec2(t1[0],t2[1]);
+            //     t4 = vec2(t2[0],t1[1]);
+            //     //console.log(t1,t2,t3,t4);
+            //     gl.bufferSubData(gl.ARRAY_BUFFER, 8*index_sq, flatten(t1));
+            //     gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_sq+1), flatten(t2));
+            //     gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_sq+2), flatten(t3));
+            //     gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_sq+3), flatten(t4));
+            //     gl.bindBuffer( gl.ARRAY_BUFFER, colorBuffer);
+            //     index_sq += 4;
+            //     colors = hexToVec4(document.getElementById("color").value);
+                
+            //     t = vec4(colors);
+
+            //     gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-4), flatten(t));
+            //     gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-3), flatten(t));
+            //     gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-2), flatten(t));
+            //     gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-1), flatten(t));
+            //     render()
+            // }
+            
+            
+        } else if (document.getElementById("rectangle").checked) {
+            gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer);
+            if(first) {
+                first = false;
+                gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer)
+                t1 = vec2(2*event.clientX/canvas.width-1, 
+                    2*(canvas.height-event.clientY)/canvas.height-1);
+                console.log(t1);
+            }
+            else {
+                first = true;
+                t2 = vec2(2*event.clientX/canvas.width-1, 
+                    2*(canvas.height-event.clientY)/canvas.height-1);
+                t3 = vec2(t1[0], t2[1]);
+                t4 = vec2(t2[0], t1[1]);
+
+                gl.bufferSubData(gl.ARRAY_BUFFER, 8*index_sq, flatten(t1));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_sq+1), flatten(t3));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_sq+2), flatten(t2));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_sq+3), flatten(t4));
+                gl.bindBuffer( gl.ARRAY_BUFFER, colorBuffer);
+                index_sq += 4;
+
+                colors = hexToVec4(document.getElementById("color").value);
+                
+                t = vec4(colors);
+
+                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-4), flatten(t));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-3), flatten(t));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-2), flatten(t));
+                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_sq-1), flatten(t));
+                render();
+            }
         } else if (document.getElementById("polygon").checked) {
             t  = vec2(2*event.clientX/canvas.width-1, 2*(canvas.height-event.clientY)/canvas.height-1);
             gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer);
@@ -133,9 +241,19 @@ window.onload = function init() {
         
             window.requestAnimFrame(render);
         } else if (document.getElementById("square").checked) {
-            
+            gl.clear( gl.COLOR_BUFFER_BIT );
+
+            for(var i = 0; i<index_sq; i+=4)
+                gl.drawArrays( gl.TRIANGLE_FAN, i, 4 );
+
+            window.requestAnimFrame(render);
         } else if (document.getElementById("rectangle").checked) {
-            
+            gl.clear( gl.COLOR_BUFFER_BIT );
+
+            for(var i = 0; i<index_sq; i+=4)
+                gl.drawArrays( gl.TRIANGLE_FAN, i, 4 );
+
+            window.requestAnimFrame(render);
         } else if (document.getElementById("polygon").checked) {
             gl.clear(gl.COLOR_BUFFER_BIT);
             for (let i = 0; i < countPolygon; i++) {
