d7ff280891f36aac514a70c99afc096a1c4b4d79
gdeananthap <gdeanantha33@gmail.com>
2022-02-27 19:53:59 +0700

    change program structure

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var canvas;
 var gl;
+var state = "draw";
 
 var maxNumLines = 200;  
 var maxNumVertices  = 2 * maxNumLines;
@@ -13,15 +14,11 @@ var countIndices = [0];
 var start = [0];
 var cIndex = 0;
 
-var colors; //= [
-//     vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
-//     vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
-//     vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-//     vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
-//     vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-//     vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-//     vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
-// ];
+var countShape = 0;
+var shapeToDraw = [];
+var vertices = []
+var vertexLeft = 2;
+var colors; 
 
 window.onload = function init() {
     canvas = document.getElementById( "c" );
@@ -57,60 +54,61 @@ window.onload = function init() {
     gl.vertexAttribPointer(verticesColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(verticesColor);
 
-    // var m = document.getElementById("mymenu");
-    
-    // m.addEventListener("click", function() {
-    //    cIndex = m.selectedIndex;
-    //     });
-
-
     canvas.addEventListener("mousedown", function(event){
-        if (document.getElementById("line").checked) {
-            gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer);
-            if(first) {
-                first = false;
-                gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer)
-                t1 = vec2(2*event.clientX/canvas.width-1, 
-                    2*(canvas.height-event.clientY)/canvas.height-1);
-                
-            }
-            else {
-                console.log("line");
-                first = true;
-                t2 = vec2(2*event.clientX/canvas.width-1, 
-                    2*(canvas.height-event.clientY)/canvas.height-1);
-
-                gl.bufferSubData(gl.ARRAY_BUFFER, 8*index_line, flatten(t1));
-                gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_line+1), flatten(t2));
-                gl.bindBuffer( gl.ARRAY_BUFFER, colorBuffer);
-                index_line += 2;
-
-                colors = hexToVec4(document.getElementById("color").value);
-                
-                t = vec4(colors);
-
-                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_line-2), flatten(t));
-                gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_line-1), flatten(t));
-                render();
-            }
-        // } else if (document.getElementById("square").checked) {
-            // function square
-        // } else if (document.getElementById("rectangle").checked) {
-            //function rectangle
-        } else if (document.getElementById("polygon").checked) {
-            t  = vec2(2*event.clientX/canvas.width-1, 2*(canvas.height-event.clientY)/canvas.height-1);
-            gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer);
-            gl.bufferSubData(gl.ARRAY_BUFFER, 8*index_polygon, flatten(t));
-
-            colors = hexToVec4(document.getElementById("color").value);
+        if (state === "draw") {
+            if(document.getElementById("line").checked){
+                gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer);
+                if(first) {
+                    first = false;
+                    gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer)
+                    t1 = vec2(2*event.clientX/canvas.width-1, 
+                        2*(canvas.height-event.clientY)/canvas.height-1);
+                    vertices.push(t1);
+                }
+                else {
+                    first = true;
+                    t2 = vec2(2*event.clientX/canvas.width-1, 
+                        2*(canvas.height-event.clientY)/canvas.height-1);
+                    vertices.push(t2);
+                    colors = hexToVec4(document.getElementById("color").value);
+                    t = vec4(colors);
+                    countShape++;
+                    var obj = new Shape(countShape, "line", vertices, t)
+                    vertices = []
+                    shapeToDraw.push(obj);
+                    console.log(shapeToDraw);
     
-            t = vec4(colors);
-            
-            gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-            gl.bufferSubData(gl.ARRAY_BUFFER, 16*index_polygon, flatten(t));
-
-            countIndices[countPolygon]++;
-            index_polygon++;
+                    gl.bufferSubData(gl.ARRAY_BUFFER, 8*index_line, flatten(t1));
+                    gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index_line+1), flatten(t2));
+                    gl.bindBuffer( gl.ARRAY_BUFFER, colorBuffer);
+    
+                    index_line += 2;
+                    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_line-2), flatten(t));
+                    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index_line-1), flatten(t));
+                    render();
+                }
+            // } else if (document.getElementById("square").checked ) {
+                // function square
+            // } else if (document.getElementById("rectangle").checked) {
+                //function rectangle
+            }
+            else if (document.getElementById("polygon").checked ) {
+                t  = vec2(2*event.clientX/canvas.width-1, 2*(canvas.height-event.clientY)/canvas.height-1);
+                gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer);
+                gl.bufferSubData(gl.ARRAY_BUFFER, 8*index_polygon, flatten(t));
+    
+                colors = hexToVec4(document.getElementById("color").value);
+        
+                t = vec4(colors);
+                
+                gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+                gl.bufferSubData(gl.ARRAY_BUFFER, 16*index_polygon, flatten(t));
+    
+                countIndices[countPolygon]++;
+                index_polygon++;
+            }
+        }else if(state = "select"){
+            //
         }
     })
     
@@ -124,10 +122,39 @@ window.onload = function init() {
                 }
     })
 
+    document.getElementById("select")
+            .addEventListener("click", function(){
+                document.getElementById("line").checked = false;
+                document.getElementById("square").checked = false;
+                document.getElementById("rectangle").checked = false;
+                document.getElementById("polygon").checked = false;
+                state = "select";
+            })
+
+    document.getElementById("line").addEventListener('click', () => {
+        state = "draw";
+        vertexLeft = 2;
+    })
+    document.getElementById("square").addEventListener('click', () => {
+        state = "draw";
+    })
+    document.getElementById("rectangle").addEventListener('click', () => {
+        state = "draw";
+    })
+    document.getElementById("polygon").addEventListener('click', () => {
+        state = "draw";
+    })
+
     function render() {
+        // gl.clear( gl.COLOR_BUFFER_BIT );
+        // for (shape in shapeToDraw){
+        //     const data = shape.getData();
+        //     if(data.type === "line"){
+                
+        //     }
+        // }
         if (document.getElementById("line").checked) {
-            gl.clear( gl.COLOR_BUFFER_BIT );
-        
+
             for(var i = 0; i<index_line; i+=2)
                 gl.drawArrays( gl.LINE_STRIP, i, 2 );
         
---
+++ shape.js
@@ -0,0 +1,216 @@
+class Shape {
+    constructor(id, type, vertices, color) {
+        this.id = id,
+        this.type = type,
+        this.vertices = vertices,
+        this.color = color,
+        this.isSelected = false
+    }
+
+    getData() {
+        const shape = {
+            id : this.id,
+            type : this.type,
+            vertices : this.vertices,
+            color : this.color,
+            isSelected : this.isSelected
+        }
+        return shape
+    }
+
+    setData(shape) {
+        this.id = shape.id,
+        this.type = shape.type,
+        this.vertices = shape.vertices,
+        this.color = shape.color,
+        this.isSelected = shape.isSelected
+    }
+    
+    computeAnchorPoint() {
+        if (this.va && this.va.length % 2 === 0) {
+            let sigmaX = 0
+            let sigmaY = 0
+            for (let i = 0; i < this.va.length; i += 2) {
+                sigmaX += this.va[i]
+                sigmaY += this.va[i+1]
+            }
+            this.anchorPoint = [sigmaX / (this.va.length/2), sigmaY / (this.va.length/2)]
+        }
+    }
+
+    assignId(id) { this.id = id }
+    setColor(color) { this.color = color }
+    setSelected(isSelected) {
+        this.isSelected = isSelected
+    }
+    deselect() {
+        this.isSelected = false
+    }
+
+    coloring(r, g, b, a) {
+        this.setColor([r,g,b,a]);
+    }
+    
+    // bind() {
+    //     const gl = this.gl
+    //     const buf = gl.createBuffer()
+    //     gl.bindBuffer(gl.ARRAY_BUFFER, buf)
+    //     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.va), gl.STATIC_DRAW)
+    //     this.vab = buf
+    //     const indexBuffer = gl.createBuffer()
+    //     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+    //     const indices = []
+    //     if (this.type != gl.LINES) {
+    //         for (let i = 2; i < this.va.length/2; i++) {
+    //             indices.push(i-1)
+    //             indices.push(i)
+    //             indices.push(0)
+    //         }
+    //     } else {
+    //         indices.push(0)
+    //         indices.push(1)
+    //     }
+    //     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW)
+    //     this.ia_length = indices.length
+
+    // }
+    
+
+    // draw(withProgram?: WebGLProgram) {
+    //     this.bind()
+    //     const program = withProgram || this.shader
+    //     const gl = this.gl
+    //     gl.useProgram(program)
+    //     const vertexPos = gl.getAttribLocation(program, 'attrib_vertexPos')
+    //     const uniformCol = gl.getUniformLocation(program, 'u_fragColor')
+    //     const uniformPos = gl.getUniformLocation(program, 'u_pos')
+    //     gl.uniformMatrix3fv(uniformPos, false, this.projectionMatrix)
+    //     gl.vertexAttribPointer(
+    //         vertexPos,
+    //         2, // it's 2 dimensional    
+    //         gl.FLOAT,
+    //         false,
+    //         0,
+    //         0
+    //     )
+    //     gl.enableVertexAttribArray(vertexPos)
+    //     if (this.color) {
+    //         gl.uniform4fv(uniformCol, this.color)
+    //     }
+    //     gl.drawElements(this.type, this.ia_length, gl.UNSIGNED_SHORT, 0)
+    // }
+
+    // drawSelect(selectProgram: WebGLProgram) {
+    //     this.bind()
+    //     const gl = this.gl
+    //     const id = this.id
+    //     gl.useProgram(selectProgram)
+    //     const vertexPos = gl.getAttribLocation(selectProgram, 'a_Pos')
+    //     const uniformCol = gl.getUniformLocation(selectProgram, 'u_id')
+    //     const uniformPos = gl.getUniformLocation(selectProgram, 'u_pos')
+    //     gl.uniformMatrix3fv(uniformPos, false, this.projectionMatrix)
+    //     gl.vertexAttribPointer(
+    //         vertexPos,
+    //         2, // it's 2 dimensional
+    //         gl.FLOAT,
+    //         false,
+    //         0,
+    //         0
+    //     )
+    //     gl.enableVertexAttribArray(vertexPos)
+    //     const uniformId = [
+    //         ((id >> 0) & 0xFF) / 0xFF,
+    //         ((id >> 8) & 0xFF) / 0xFF,
+    //         ((id >> 16) & 0xFF) / 0xFF,
+    //         ((id >> 24) & 0xFF) / 0xFF,
+    //     ]
+    //     gl.uniform4fv(uniformCol, uniformId)
+    //     gl.drawElements(this.type, this.ia_length, gl.UNSIGNED_SHORT, 0)
+    // }
+
+    // drawPoint(vertPointProgram: WebGLProgram) {
+    //     this.bind()
+    //     const program = vertPointProgram
+    //     const gl = this.gl
+    //     gl.useProgram(program)
+    //     const vertexPos = gl.getAttribLocation(program, 'a_Pos')
+    //     const uniformCol = gl.getUniformLocation(program, 'u_fragColor')
+    //     const uniformPos = gl.getUniformLocation(program, 'u_pos')
+    //     const resolutionPos = gl.getUniformLocation(program, 'u_resolution')
+    //     gl.uniformMatrix3fv(uniformPos, false, this.projectionMatrix)
+    //     gl.vertexAttribPointer(
+    //         vertexPos,
+    //         2, // it's 2 dimensional
+    //         gl.FLOAT,
+    //         false,
+    //         0,
+    //         0
+    //     )
+    //     gl.uniform2f(resolutionPos, gl.canvas.width, gl.canvas.height)
+    //     gl.enableVertexAttribArray(vertexPos)
+    //     if (this.color) {
+    //         gl.uniform4fv(uniformCol, this.color)
+    //     }
+    //     gl.uniform4fv(uniformCol, [0.0, 0.0, 1.0, 1.0])
+    //     if (this.ia_length > 3) {
+    //         gl.drawElements(gl.POINTS, 1, gl.UNSIGNED_SHORT, 2)
+    //         gl.drawElements(gl.POINTS, 1, gl.UNSIGNED_SHORT, 4)
+    //         gl.drawElements(gl.POINTS, 1, gl.UNSIGNED_SHORT, 0)
+    //         for (let i = 3; i < this.ia_length; i+=3) {
+    //             gl.drawElements(gl.POINTS, 1, gl.UNSIGNED_SHORT, (i+1)*2)
+    //         }
+    //     } else {
+    //         gl.drawElements(gl.POINTS, 2, gl.UNSIGNED_SHORT, 0)
+    //     }
+    // }
+
+    // drawPointSelect(selectProgram: WebGLProgram) {
+    //     this.bind()
+    //     const gl = this.gl
+    //     gl.useProgram(selectProgram)
+    //     const vertexPos = gl.getAttribLocation(selectProgram, 'a_Pos')
+    //     const uniformCol = gl.getUniformLocation(selectProgram, 'u_id')
+    //     const uniformPos = gl.getUniformLocation(selectProgram, 'u_pos')
+    //     const resolutionPos = gl.getUniformLocation(selectProgram, 'u_resolution')
+    //     gl.uniformMatrix3fv(uniformPos, false, this.projectionMatrix)
+    //     gl.vertexAttribPointer(
+    //         vertexPos,
+    //         2, // it's 2 dimensional
+    //         gl.FLOAT,
+    //         false,
+    //         0,
+    //         0
+    //     )
+    //     gl.enableVertexAttribArray(vertexPos)
+    //     gl.uniform2f(resolutionPos, gl.canvas.width, gl.canvas.height)
+    //     if (this.ia_length > 3) {
+    //         gl.uniform4fv(uniformCol, setElementId(2))
+    //         gl.drawElements(gl.POINTS, 1, gl.UNSIGNED_SHORT, 0)
+    //         gl.uniform4fv(uniformCol, setElementId(3))
+    //         gl.drawElements(gl.POINTS, 1, gl.UNSIGNED_SHORT, 2)
+    //         gl.uniform4fv(uniformCol, setElementId(1))
+    //         gl.drawElements(gl.POINTS, 1, gl.UNSIGNED_SHORT, 4)
+    //         let lastVertId = 4
+    //         for (let i = 3; i < this.ia_length; i+=3) {
+    //             gl.uniform4fv(uniformCol, setElementId(lastVertId))
+    //             gl.drawElements(gl.POINTS, 1, gl.UNSIGNED_SHORT, (i+1)*2)
+    //             lastVertId++
+    //         }
+    //     } else {
+    //         gl.uniform4fv(uniformCol, setElementId(1))
+    //         gl.drawElements(gl.POINTS, 1, gl.UNSIGNED_SHORT, 0)
+    //         gl.uniform4fv(uniformCol, setElementId(2))
+    //         gl.drawElements(gl.POINTS, 1, gl.UNSIGNED_SHORT, 2)
+    //     }        
+    // }
+}
+
+// function setElementId(id: number) {
+//     const uniformId = [
+//         ((id >> 0) & 0xFF) / 0xFF,
+//         ((id >> 8) & 0xFF) / 0xFF,
+//         ((id >> 16) & 0xFF) / 0xFF,
+//         0x69,
+//     ]
+//     return uniformId
+// }
